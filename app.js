818049dc8ccb300a823a71e2488cb5256f582926
londondreams27 <88943110+londondreams27@users.noreply.github.com>
2023-05-21 17:14:05 +0530

    Traverse the DOM
    Answers for task 7/17

--- app.js
+++ app.js
@@ -1,26 +1,110 @@
-//getElementsByclassName//
 
-// var items= document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent = 'Hello 2';
-// items[1].style.backgroundColor = 'green';    //change bg color of 2nd item to green
 
-//querySelector//
- 
-var header = document.querySelector('#main-header');
+// code to traverse the DOM 
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.color = 'green';  //change text color of 2nd item to green
 
-//queryselectAll
+// Traversing the DOM //  task 7/17
 
-// var titles = document.querySelectorAll('.title');
-// console.log(titles);
+//var itemList = document.querySelector('#items');
 
-// var odd = document.querySelectorAll('li:nth-child(odd)'); //css selector
-// for (var i =0; i<odd.length ; i++)
-// {
-//     odd[i].style.backgroundColor = 'green';
+//parentNode property
 
-// }
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+//parentNode and parentElement can be used interchangeably
+
+//  console.log(itemList.parentElement);
+//  itemList.parentElement.style.backgroundColor = '#f4f4f4';
+//  console.log(itemList.parentElement.parentElement.parentElement);
+
+//childNode
+//console.log(itemList.childNodes); //it also counts the white space or ine spaces in the html file(returns node list)
+
+//use children instead
+// console.log(itemList.children); //returns an html collection
+// console.log(itemList.children[1]);
+
+//firstChild//
+// console.log(itemList.firstChild); // also counts any line breaks and white spaces
+
+//firstElementChild
+// console.log(itemList.firstElementChild); //returns the actual list item
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+
+// //lastChild//
+// // console.log(itemList.lastChild); // also counts any line breaks and white spaces
+
+// //lastElementChild
+// console.log(itemList.lastElementChild); //returns the actual list item
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+// //nextsibling
+// //console.log(itemList.nextSibling); // returns Null if no next sibling is there
+// //nextElemntSibling
+// console.log(itemList.nextElementSibling);
+
+// //previoussibling
+// //console.log(itemList.previousSibling); // returns Null if no next sibling is there
+// //previousElemntSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.getElementsByClassName.color = 'green';
+
+
+//createElement - add Hello World before item Lister 
+
+//create a div
+// var newDiv = document.createElement('div');
+
+// //add class
+// newDiv.className  = 'Hello';
+
+// //add id
+// newDiv.id = 'hello1';
+
+// //add attribute
+// newDiv.setAttribute('title','Hello Div');
+
+// //create text node
+// var newDivText = document.createTextNode('HEllo World');
+
+// //add text to div
+// newDiv.appendChild(newDivText);
+
+// //inserting in the DOM
+
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
+
+// console.log(newDiv);
+
+// container.insertBefore(newDiv, h1);
+
+===============================================================================================
+// Add hello World before Item1 
+// //create a new class 
+// var newItem = document.createElement('class');
+
+// //add id
+// newItem.id = 'hello1';
+
+// //add attribute
+// newItem.setAttribute('title','Hello Div');
+
+// //create text node
+// var newItemText = document.createTextNode('HEllo World');
+
+// //add text to item
+// newItem.appendChild(newItemText);
+
+// //inserting in the DOM 
+
+// var item = document.querySelector('ul .list-group');
+// var li = document.querySelector('li');
+
+// console.log(newItem);
+
+// itemList.insertBefore(newItem, li);
+

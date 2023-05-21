88e9f291c0cbb9b52b64328679deacdeffd14950
londondreams27 <88943110+londondreams27@users.noreply.github.com>
2023-05-21 15:26:33 +0530

    Update app.js
    for task 6/17

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('I love git')
+//getElementsByclassName//
+
+// var items= document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].style.backgroundColor = 'green';    //change bg color of 2nd item to green
+
+//querySelector//
+ 
+var header = document.querySelector('#main-header');
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'green';  //change text color of 2nd item to green
+
+//queryselectAll
+
+// var titles = document.querySelectorAll('.title');
+// console.log(titles);
+
+// var odd = document.querySelectorAll('li:nth-child(odd)'); //css selector
+// for (var i =0; i<odd.length ; i++)
+// {
+//     odd[i].style.backgroundColor = 'green';
+
+// }

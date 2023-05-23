f7612e6255fd4c12f0e3ff39a6dc26a1bff8b29d
londondreams27 <88943110+londondreams27@users.noreply.github.com>
2023-05-23 23:13:47 +0530

    Update app.js
    task 9/17: adding descriptions to list items
    search and filter methods

--- app.js
+++ app.js
@@ -171,5 +171,47 @@ function removeItem(e){
     if(confirm('Are You Sure?')){
       var li = e.target.parentElement;                                      //deliverable 1
       itemList.removeChild(li);
+      
+      
+ //=======================================================================================/task 9/17//
+      
+ // getting  description from  the form 
+     
+  var newItem = document.getElementById('item').value;
+  var desc = document.getElementById('description').value;
+      
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(" "));
+  li.appendChild(document.createTextNode(desc));
+
+  
+//filtering and searching
+//Filter event
+filter.addEventListener('keyup', filterItems);
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
+      
+      
+      
+      
     }
 
